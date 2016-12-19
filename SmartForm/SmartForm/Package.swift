cb4fa1ad24f12a6bee39c152fad535d05135287d
Pushan <pusmitra@in.ibm.com>
2016-12-19 15:41:51 +0530

    Started SmartForm structure

---
+++ SmartForm/SmartForm/Package.swift
@@ -0,0 +1,14 @@
+//
+//  Package.swift
+//  SmartForm
+//
+//  Created by Pushan Mitra on 19/12/16.
+//  Copyright © 2016 Pushan Mitra. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "SmartForm",
+    dependencies: []
+)
