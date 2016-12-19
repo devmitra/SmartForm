e9b7e7cffc0a02810c7241f3cb9544de3363f482
Pushan <pusmitra@in.ibm.com>
2016-12-19 12:34:22 +0530

    Adding git ignore

---
+++ SmartForm/SmartFormLib/SmartFormLib.h
@@ -0,0 +1,19 @@
+//
+//  SmartFormLib.h
+//  SmartFormLib
+//
+//  Created by Pushan Mitra on 19/12/16.
+//  Copyright © 2016 Pushan Mitra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SmartFormLib.
+FOUNDATION_EXPORT double SmartFormLibVersionNumber;
+
+//! Project version string for SmartFormLib.
+FOUNDATION_EXPORT const unsigned char SmartFormLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SmartFormLib/PublicHeader.h>
+
+
